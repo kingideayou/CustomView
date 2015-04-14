c23b41c6b22560cf5e9d2c3a042725499632e2bc
NeXT <kingideayou@qq.com>
2015-04-14 10:53:55 +0800

    fist commit

---
+++ app/src/main/java/com/next/customview/PieChart.java
@@ -0,0 +1,15 @@
+package com.next.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by NeXT on 15-4-14.
+ */
+public class PieChart extends View {
+
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
